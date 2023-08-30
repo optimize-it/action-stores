1da748acc3e3901f3d1cfcafed0fcd6938ba5cfc
Sanket Nagdive <sanket.r@optit.in>
2023-08-30 16:40:00 +0530

    actions,models for few azure resources

---
+++ azure-compute/actions/nic_actions.py
@@ -0,0 +1,67 @@
+import logging
+
+from .. import action_store as action_store
+from ..azure_wrapper import *
+# from ..models.vmss_models import *
+from ..models.networkInterface_models import *
+from typing import Union
+
+logger = logging.getLogger(__name__)
+
+@action_store.kubiya_action()
+def create_or_update_azure_networkInterface(params: networkInterfaceCreationParameters) -> Union[networkInterfaceModel, dict]:
+    networkInterfaceName = params.networkInterfaceName
+    subscriptionId = params.subscriptionId
+    resourceGroupName = params.resourceGroupName
+    vmName = params.vmName
+    networkInterface_data = {
+                            "properties": {
+                                "enableAcceleratedNetworking": true,
+                                "disableTcpStateTracking": true,
+                                "ipConfigurations": [
+                                {
+                                    "name": "ipconfig1",
+                                    "properties": {
+                                    "subnet": {
+                                        "id": "/subscriptions/{params.subscriptionId}/resourceGroups/{params.resourceGroupName}/providers/Microsoft.Network/virtualNetworks/{params.vnetname}/subnets/{params.subnet_name}"
+                                    }
+                                    }
+                                }
+                                ]
+                            },
+                            "location": params.location
+                            }
+    endpoint = f"subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/networkInterfaces/{networkInterfaceName}"
+    api_version = "2023-02-01"
+    response_data = put_wrapper(endpoint, subscriptionId, api_version, data=networkInterface_data)
+    return networkInterfaceModel(**response_data)
+
+@action_store.kubiya_action()
+def get_azure_networkInterfaces(params: networkInterfaceCreationParameters) -> Union[networkInterfaceModel, dict]:
+    subscriptionId = params.subscriptionId
+    resourceGroupName = params.resourceGroupName
+    networkInterfaceName = params.networkInterfaceName
+    endpoint = f"subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/networkInterfaces/{networkInterfaceName}"
+    api_version = "2022-11-01"
+
+    response_data = get_wrapper(endpoint, params.subscriptionId, api_version)
+
+    return networkInterfaceModel(**response_data)
+
+@action_store.kubiya_action()
+def listall_azure_networkInterfaces(params: networkInterfaceListParameters) -> Union[networkInterfaceListModel, dict]:
+    #subscriptionId = params.subscriptionId
+    endpoint = f"subscriptions/{params.subscriptionId}/providers/Microsoft.Compute/networkInterfaces"
+    api_version = "2022-11-01"
+    response_data = get_wrapper(endpoint, params.subscriptionId, api_version)
+    networkInterface_list = response_data.get('value', [])
+    return [networkInterfaceListModel(**networkInterface) for networkInterface in networkInterface_list]
+
+def list_by_rg_azure_networkInterfaces(params: networkInterfaceListParameters) -> Union[networkInterfaceListModel, dict]:
+    #subscriptionId = params.subscriptionId
+    endpoint = f"subscriptions/{params.subscriptionId}/resourceGroups/{params.resourceGroupName}/providers/Microsoft.Compute/networkInterfaces"
+    api_version = "2022-11-01"
+    response_data = get_wrapper(endpoint, params.subscriptionId, api_version)
+    networkInterface_list = response_data.get('value', [])
+    return [networkInterfaceListModel(**networkInterface) for networkInterface in networkInterface_list]
+    
---
+++ azure-compute/models/vmss_models.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class VMSSCreationParameters(BaseModel):
+    subscriptionId: str
+    resourceGroupName: str
+    vmName: str
+    networkInterfaceName: str
+    location: str
+    vmSize: str
+    computer_name: str
+    admin_username: str
+    admin_password: str
+    publisher: str
+    offer: str
+    sku: str
+    version: str
+
+
+class VirtualMachineSSResponseModel(BaseModel):
+    name: Optional[str]
+    id: Optional[str]
+    type: Optional[str]
+    location: Optional[str]
+    tags: Optional[dict]
+    properties: dict
+
+
+class VMSSQueryParameters(BaseModel):
+    subscriptionId: str
+    resourceGroupName: str
+    vmssName: str
+
+
+class VirtualMachineSSListModel(BaseModel):
+    id: str
+    name: str
+    type: str
+    location: str
+    properties: dict
+    tags: Optional[dict] = {}
+
+
+class VMSSListParameters(BaseModel):
+    subscriptionId: str
+
+class VMSSGetParametersByLocation(BaseModel):
+    subscriptionId: str
+    resourceGroupName: str
+    vmssName: str
+    location: str
+    
+
